c882039868bb70e29a9af9a48d8c04c4961b94b4
vanessavr <110788192+vanessavr@users.noreply.github.com>
2024-04-06 20:24:46 -0500

    allowed ico

--- middleware.ts
+++ middleware.ts
@@ -60,5 +60,5 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-    matcher: ['/((?!api|_next/static|_next/image|.*\\.webp$|.*\\.mp3|.*\\.(?:png|jpg|jpeg|gif)$).*)'],
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.webp$|.*\\.mp3|.*\\.(?:png|jpg|jpeg|gif|ico)$).*)'],
 }
